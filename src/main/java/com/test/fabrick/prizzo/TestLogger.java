aeeefbbb1f3de709a4495059edc59a6215f00b0e
V3X15 <paolo.rizzo2798@gmail.com>
2025-03-30 21:12:09 +0200

    prizzo
    Migliorie geneali

---
+++ src/main/java/com/test/fabrick/prizzo/TestLogger.java
@@ -0,0 +1,72 @@
+package com.test.fabrick.prizzo;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestLogger {
+	
+	private static Logger logger = null;
+    private static JSONArray logArray = new JSONArray();
+    private static final String LOG_FILE_PREFIX = "logs";
+    private static final String LOG_FILE_EXTENSION = ".json";
+    private static String logDirectory = "target/logs"; 
+
+    public TestLogger(Class<?> clazz) {
+        logger = LoggerFactory.getLogger(clazz);
+    }
+
+    public static void setLogDirectory(String path) {
+        logDirectory = path;
+    }
+
+    private void logToJson(String level, String message) {
+        JSONObject logEntry = new JSONObject();
+        logEntry.put("timestamp", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
+        logEntry.put("level", level);
+        logEntry.put("message", message);
+        
+        logArray.put(logEntry);
+        saveLogsToFile();
+    }
+
+    private void saveLogsToFile() {
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss"));
+        String logFileName = LOG_FILE_PREFIX + " - " + timestamp + LOG_FILE_EXTENSION;
+        String logFilePath = Paths.get(logDirectory, logFileName).toString();
+        
+        try (FileWriter file = new FileWriter(logFilePath)) {
+            file.write(logArray.toString(4)); 
+            file.flush();
+        } catch (IOException e) {
+            logger.error("Errore durante la scrittura dei log su file", e);
+        }
+    }
+
+    public void info(String message) {
+        logger.info(message);
+        logToJson("INFO", message);
+    }
+    
+    public void warn(String message) {
+        logger.warn(message);
+        logToJson("WARN", message);
+    }
+    
+    public void error(String message) {
+        logger.error(message);
+        logToJson("ERROR", message);
+    }
+    
+    public void debug(String message) {
+        logger.debug(message);
+        logToJson("DEBUG", message);
+    }
+}
--- src/test/java/com/test/fabrick/prizzo/FabrickControllerTest.java
+++ src/test/java/com/test/fabrick/prizzo/FabrickControllerTest.java
@@ -21,6 +21,7 @@ public class FabrickControllerTest {
     private FabrickService service;
 
     private static final String ACCOUNT_ID = "14537780";
+    private static final TestLogger tLogger = new TestLogger(FabrickControllerTest.class);  // loggin + scrittura su file
     private static final Logger logger = LoggerFactory.getLogger(FabrickControllerTest.class);
 
     @Test
@@ -35,6 +36,21 @@ public class FabrickControllerTest {
 	        
     	 }catch (Exception e) {
  			logger.error("ERRORE: ", e.getMessage());
+ 		}
+    }
+    
+    @Test
+    public void testBalanceInMemory() throws Exception {
+    	try {
+    		
+	        ResponseEntity<String> response = service.getBalance(ACCOUNT_ID);
+	        assertEquals(HttpStatus.OK, response.getStatusCode());
+	
+	        JSONObject jsonResponse = new JSONObject(response.getBody());
+	        tLogger.debug("Response della chiamata balance : " + jsonResponse.toString());
+	        
+    	 }catch (Exception e) {
+    		tLogger.error("ERRORE: " + e.getMessage());
  		}
     }
 
@@ -55,6 +71,26 @@ public class FabrickControllerTest {
         
 	    }catch (Exception e) {
 			logger.error("ERRORE: ", e.getMessage());
+		}
+    }
+    
+    @Test
+    public void testGetTransactionsInMemory() throws Exception {
+    	try {
+	        String fromAccountingDate = "2019-01-01";
+	        String toAccountingDate   = "2019-12-01";
+	        		
+	        ResponseEntity<String> response = service.getTransactions(ACCOUNT_ID, fromAccountingDate, toAccountingDate);
+	        assertEquals(HttpStatus.OK, response.getStatusCode());
+	
+	        JSONObject jsonResponse = new JSONObject(response.getBody());
+	        JSONObject payload = jsonResponse.getJSONObject("payload");
+	
+	        assertEquals(true, payload.has("list"));
+	        tLogger.debug("Response della chiamata transactions : " + jsonResponse.toString());
+        
+	    }catch (Exception e) {
+	    	tLogger.error("ERRORE: " + e.getMessage());
 		}
     }
 
@@ -86,6 +122,37 @@ public class FabrickControllerTest {
 	        
     	}catch (Exception e) {
     		logger.error("ERRORE: ", e.getMessage());
+		}
+    }
+    
+    @Test
+    public void testMoneyTransferInMemory() throws Exception {
+    	try {
+    		
+	        Map<String, Object> request = new HashMap<>();
+	        Map<String, Object> creditor = new HashMap<>();
+	        Map<String, Object> account = new HashMap<>();
+	
+	        account.put("accountCode", "IT23A0336844430152923804660");
+	        
+	        creditor.put("name", "John Doe");
+	        creditor.put("account", account);
+	
+	        request.put("creditor", creditor);
+	        request.put("description", "Payment invoice 75/2025");
+	        request.put("amount", "100");
+	        request.put("currency", "EUR");
+	        request.put("executionDate", "2025-03-26");
+	
+	        tLogger.debug("Request body: " + request);
+	
+	        ResponseEntity<String> response = service.makeMoneyTransfer(ACCOUNT_ID, request);
+	        assertEquals(HttpStatus.OK, response.getStatusCode());
+	
+	        tLogger.debug("Response della chiamata moneyTransfer : " + response.getBody());
+	        
+    	}catch (Exception e) {
+    		tLogger.error("ERRORE: " + e.getMessage());
 		}
     }
 }
